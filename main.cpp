f3edce4184c337cfb0e4b16ee647fa64e01b474f
Lauren358 <ldavis482@ivytech.edu>
2026-05-08 17:25:54 -0400

    Add namespace and purpose of program
    Added program description and author information.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
 #include "sqlite3.h"
+using namespace std;
+
+/* Program name: Database Connection and Callbacks
+*  Author: Lauren Davis
+*  Date last updated: 5/8/2026
+* Purpose: This program opens a connection to the Chinook database. For each album a track listing will be shown and each album will appear in the results once. If the connection doesn't open successfully an error message will print and end the program, if it opens successfully, a success message will print when it.
+*/
 
 int callback(void *data, int argc, char **argv, char **azColName);
 
