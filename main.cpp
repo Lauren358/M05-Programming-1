1fa40d9413a2bbc9ae0b49ec403658dd7434b3ca
Lauren358 <ldavis482@ivytech.edu>
2026-05-08 17:19:56 -0400

    Add files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "sqlite3.h"
+
+int callback(void *data, int argc, char **argv, char **azColName);
+
+int main()
+{
+	open the database first
+	return 0;
+}
