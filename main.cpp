0925ffb2e7ff782627c79a17bfa5f14b80739ea2
Lauren358 <ldavis482@ivytech.edu>
2026-05-08 21:40:15 -0400

    Refactor database connection and output formatting
    Updated SQL query and improved output formatting for album and track display.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@ using namespace std;
 * Purpose: This program opens a connection to the Chinook database. For each album a track listing will be shown and each album will appear in the results once. If the connection doesn't open successfully an error message will print and end the program, if it opens successfully, a success message will print when it.
 */
 
+//note AI was used to aid in ensuring the connection to database code was correct. The code was throughly looked over by human eye.
 
-// Forward declaration
+
 int callback(void *data, int argc, char **argv, char **azColName);
 
+//structure remembers last album that was printed and prints the album name once without repitition
 struct AlbumState {
     int lastAlbumId = -1;
 };
@@ -25,27 +27,29 @@ int callback(void *data, int argc, char **argv, char **azColName) {
     string trackName = argv[2];         // Track Name
     string artistName = argv[3];        // Artist Name
 
-    // Print album header once
+    // Print album once
     if (albumId != state->lastAlbumId) {
         state->lastAlbumId = albumId;
-        cout << "\nAlbum: " << albumTitle
-             << " (ID: " << albumId << ")"
-             << " - Artist: " << artistName << endl;
+        
+    //Print out ID, album name, artist
+        cout << "\nAlbumId: " << albumId
+             << "\nAlbum: " << albumTitle
+             << "\nArtist: " << artistName << endl;
     }
 
     // Print track
-    cout << "  - " << trackName << endl;
+    cout << "\nTracks: " << trackName << endl;
 
     return 0;
 }
 
 int main() {
     sqlite3 *db;
-    int rc;
-
-    rc = sqlite3_open("chinook.db", &db);
-
-    if (rc != SQLITE_OK) {
+    int ret;
+//returns code from sql database
+    ret = sqlite3_open("chinook.db", &db);
+//if the database does not connect the error message will show 
+    if (ret != SQLITE_OK) {
         cout << "Error in connection: unable to open database file" << endl;
         cout << sqlite3_errmsg(db) << endl;
         return 1;
@@ -54,22 +58,22 @@ int main() {
     cout << "Database opened successfully!" << endl;
 
     const char *sql =
-        "SELECT Album.AlbumId, Album.Title, Track.Name, Artist.Name "
-        "FROM Album "
-        "JOIN Track ON Track.AlbumId = Album.AlbumId "
-        "JOIN Artist ON Album.ArtistId = Artist.ArtistId "
-        "ORDER BY Album.AlbumId;";
+        "SELECT al.AlbumId, al.Title, t.Name, art.Name "
+        "FROM albums "
+        "JOIN tracks ON t.AlbumId = al.AlbumId "
+        "JOIN artists ON al.ArtistId = art.ArtistId "
+        "ORDER BY al.AlbumId;";
 
     AlbumState state;
     char *errMsg = nullptr;
 
-    rc = sqlite3_exec(db, sql, callback, &state, &errMsg);
-
-    if (rc != SQLITE_OK) {
+    ret = sqlite3_exec(db, sql, callback, &state, &errMsg);
+//if the database does not connect the error message will show 
+    if (ret != SQLITE_OK) {
         cout << "SQL error: " << errMsg << endl;
         sqlite3_free(errMsg);
     }
-
+//closes database to not corrupt it
     sqlite3_close(db);
 
     return 0;
